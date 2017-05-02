ce9ef8e5852676f5be2e1e3ca873eb2975c08230
xach <zironside@hotmail.com>
2017-05-02 09:12:37 -0400

    add redirects list to storage

--- src/main/java/com/gti/redirects/Storage.java
+++ src/main/java/com/gti/redirects/Storage.java
@@ -5,10 +5,8 @@
  */
 package com.gti.redirects;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -19,45 +17,74 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 /**
- *
  * @author xach
  */
 public class Storage implements StorageI {
-	private String file = "redirects.json";
-	private File jsonFile;
+    private String file = "redirects.json";
+    private File jsonFile;
 
-	public Storage() {
-		File redirectFile = new File(System.getProperty("user.dir")+"/"+file);
-		try {
-			redirectFile.createNewFile();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		jsonFile = redirectFile;
-
-	}
-	@Override
-	public List redirects() {
-		return null;
-	}
-
-	@Override
-	public boolean addRedirect(Redirect redirect) {
+    public Storage() {
+        File redirectFile = new File(System.getProperty("user.dir") + "/" + file);
         try {
+            if (redirectFile.createNewFile()) {
+                JSONArray jsonArray = new JSONArray();
+                jsonFile = redirectFile;
+                writeJsonArray(jsonArray);
+            } else {
+                jsonFile = redirectFile;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public List redirects() {
+        List<Map<String, String>> redirects = new ArrayList<Map<String, String>>();
+        JSONArray jsonArray = null;
+        try {
+            jsonArray = parseFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        if (jsonArray != null) {
+            for (int i = 0; i < jsonArray.size(); i++) {
+                JSONObject object = (JSONObject) jsonArray.get(i);
+                Map<String, String> map = new HashMap<String, String>();
+                map.put("domain", object.get("domain").toString());
+                map.put("type", object.get("type").toString());
+                map.put("redirect_to", object.get("redirect_to").toString());
+
+                redirects.add(map);
+                // now do something with the Object
+            }
+        }
+        return redirects;
+    }
+
+    @Override
+    public boolean addRedirect(Redirect redirect) {
+        try {
+            System.out.println("Successfully Copied JSON Object to File...");
             JSONArray jsonArray = parseFile();
             JSONObject redirectJson = convertRedirectToJson(redirect);
-
+            jsonArray.add(redirectJson);
+            writeJsonArray(jsonArray);
+            return true;
         } catch (IOException e) {
             e.printStackTrace();
         } catch (ParseException e) {
             e.printStackTrace();
         }
         return false;
-	}
+    }
 
-	private JSONArray parseFile() throws IOException, ParseException {
+    private JSONArray parseFile() throws IOException, ParseException {
         JSONParser parser = new JSONParser();
+        System.out.println(jsonFile.getPath());
         return (JSONArray) parser.parse(new FileReader(jsonFile.getPath()));
     }
 
@@ -68,6 +95,18 @@ public class Storage implements StorageI {
         json.put("type", redirect.getType());
         json.put("redirect_to", redirect.getRedirect_to());
         return json;
+    }
+
+    private boolean writeJsonArray(JSONArray jsonArray) {
+        try (FileWriter file = new FileWriter(jsonFile.getPath())) {
+            file.write(jsonArray.toJSONString());
+            System.out.println("Successfully Copied JSON Object to File...");
+            System.out.println("\nJSON Object: " + jsonArray);
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
 }
--- src/main/java/com/gti/redirects/StorageI.java
+++ src/main/java/com/gti/redirects/StorageI.java
@@ -1,12 +1,13 @@
 package com.gti.redirects;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by xach on 5/2/17.
  */
 public interface StorageI {
-    List redirects();
+    List<Map<String, String>> redirects();
     boolean addRedirect(Redirect redirect);
 
 }

7a1df5dab485100b2fd5473a68f5256f89de3453
xach <zironside@hotmail.com>
2017-05-16 07:13:39 -0400

    add redirects request test

--- src/main/java/com/gti/redirects/Redirects/RedirectsRequest.java
+++ src/main/java/com/gti/redirects/Redirects/RedirectsRequest.java
@@ -1,10 +1,9 @@
 package com.gti.redirects.Redirects;
 
-import com.gti.redirects.AbstractRequest;
-import com.gti.redirects.Answer;
-import com.gti.redirects.EmptyPayload;
+import com.gti.redirects.*;
 import com.gti.redirects.Util.ViewUtil;
-import com.gti.redirects.Validable;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 import java.util.HashMap;
 import java.util.List;
@@ -15,21 +14,34 @@ import java.util.Map;
  */
 public class RedirectsRequest<V extends Validable> extends AbstractRequest {
     private Map <String, Object> templateMap;
-    public RedirectsRequest(RedirectsModel model) {
+    public RedirectsRequest(Model model) {
        super(EmptyPayload.class, model);
     }
 
     @Override
     protected Answer processImpl(Validable value, Map queryParams, boolean shouldReturnHtml) {
+        List<Map<String,String>> redirects = model.find();
         if(shouldReturnHtml) {
-            List redirects = model.find();
             Map map = new HashMap();
             map.put("redirects", redirects);
             map.put("content", "redirects/layout.hbs");
             return Answer.ok(ViewUtil.render(map, "layout.hbs"));
         } else {
-            String json = dataToJson(model);
-            return Answer.ok(json);
+
+            JSONArray jsonArray = new JSONArray();
+
+            for(Map<String, String> map : redirects) {
+                JSONObject jsonObject = new JSONObject();
+
+                jsonObject.put("id", map.get("id"));
+                jsonObject.put("domain", map.get("domain"));
+                jsonObject.put("redirect_domain", map.get("redirect_domain"));
+                jsonObject.put("use_path", map.get("use_path"));
+                jsonObject.put("status", map.get("status"));
+
+                jsonArray.add(jsonObject);
+            }
+            return Answer.ok(jsonArray.toString());
         }
     }
 }
--- src/test/java/com/gti/redirectstests/RedirectsRequestTest.java
+++ src/test/java/com/gti/redirectstests/RedirectsRequestTest.java
@@ -1,7 +1,85 @@
 package com.gti.redirectstests;
 
+import com.gti.redirects.Answer;
+import com.gti.redirects.EmptyPayload;
+import com.gti.redirects.Model;
+import com.gti.redirects.Redirects.RedirectsRequest;
+import org.easymock.EasyMock;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by xach on 5/15/17.
  */
 public class RedirectsRequestTest {
+    private List<Map<String, String>> returnFind = new ArrayList<>();
+
+
+    @Before
+    public void beforeAll() {
+        Map<String, String> returnHash = new HashMap<>();
+        returnHash.put("id", "1");
+        returnHash.put("domain", "domain.com");
+        returnHash.put("redirect_domain", "newdomain.com");
+        returnHash.put("status", "301");
+        returnHash.put("use_path", "1");
+
+        returnFind.add(returnHash);
+
+
+        Map<String, String> returnHash2 = new HashMap<>();
+        returnHash.put("id", "2");
+        returnHash.put("domain", "domain2.com");
+        returnHash.put("redirect_domain", "newdomain2.com");
+        returnHash.put("status", "302");
+        returnHash.put("use_path", "0");
+
+        returnFind.add(returnHash2);
+
+
+        Map<String, String> returnHash3 = new HashMap<>();
+        returnHash.put("id", "3");
+        returnHash.put("domain", "domain3.com");
+        returnHash.put("redirect_domain", "newdomain3.com");
+        returnHash.put("status", "404");
+        returnHash.put("use_path", "0");
+
+        returnFind.add(returnHash2);
+    }
+
+    @Test
+    public void displayRedirects() {
+        EmptyPayload emptyPayload = new EmptyPayload();
+        Assert.assertTrue(emptyPayload.isValid());
+
+        Model model = EasyMock.createNiceMock(Model.class);
+        EasyMock.expect(model.find()).andReturn(returnFind);
+        EasyMock.replay(model);
+
+        RedirectsRequest redirectsRequest = new RedirectsRequest(model);
+
+        JSONArray jsonArray = new JSONArray();
+
+        for(Map<String, String> map : returnFind) {
+            JSONObject jsonObject = new JSONObject();
+
+            jsonObject.put("id", map.get("id"));
+            jsonObject.put("domain", map.get("domain"));
+            jsonObject.put("redirect_domain", map.get("redirect_domain"));
+            jsonObject.put("use_path", map.get("use_path"));
+            jsonObject.put("status", map.get("status"));
+
+            jsonArray.add(jsonObject);
+        }
+
+        Assert.assertEquals(new Answer(200 , jsonArray.toString()), redirectsRequest.process(emptyPayload, new HashMap<>(), false));
+    }
 }

7af7d4d11e36c4a348f86571266bd09389cb292c
xach <zironside@hotmail.com>
2017-06-20 07:08:06 -0400

    add instances for admin and redirects on ports

--- src/main/java/com/gti/redirects/Main.java
+++ src/main/java/com/gti/redirects/Main.java
@@ -1,65 +1,64 @@
 package com.gti.redirects;
 
-import static spark.Spark.before;
-import static spark.Spark.get;
-import static spark.Spark.post;
+import static spark.Service.ignite;
 
 import com.github.xachman.SQLiteDatabaseHelper;
-import com.gti.redirects.Redirects.*;
 import com.gti.redirects.Redirects.Controllers.*;
 import com.gti.redirects.Redirects.Models.RedirectsModel;
 import com.gti.redirects.Redirects.Models.RedirectsTable;
-import spark.Spark;
+import spark.Service;
 
 import java.util.Base64;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
-
+	private static RedirectsModel redirectsModel;
 	/**
 	 * @param args the command line arguments
 	 */
 	public static void main(String[] args) {
-		Spark.port(4000);
-		Spark.staticFileLocation("/public");
-		RedirectsModel redirectsModel = new RedirectsModel(new RedirectsTable(), new SQLiteDatabaseHelper(System.getProperty("user.dir")+"/database.db"));
-		Spark.exception(Exception.class, (exception, request, response) -> {
+		redirectsModel = new RedirectsModel(new RedirectsTable(), new SQLiteDatabaseHelper(System.getProperty("user.dir")+"/database.db"));
+		startAdminInstance();
+		startRedirectInstance();
+	}
+
+	static void startAdminInstance() {
+		Service http = ignite().port(4000);
+
+		http.staticFileLocation("/public");
+		http.exception(Exception.class, (exception, request, response) -> {
 			exception.printStackTrace();
 		});
-		before("/*", (request, response) -> {
-
-			if (!request.host().equals(System.getenv("HOST")) && !request.pathInfo().equals("/")) {
-				request.session(true);
-				request.session().attribute("pathInfo", request.pathInfo());
-				response.redirect("/");
-			}
-		});
-		before("/admin/*", (request, response) -> {
+		http.before("/admin/*", (request, response) -> {
 			Boolean authenticated = false;
 			String auth = request.headers("Authorization");
 			if (auth != null && auth.startsWith("Basic")) {
-                String b64Credentials = auth.substring("Basic".length()).trim();
-                String credentials = new String(Base64.getDecoder().decode(b64Credentials));
-                if (credentials.equals(System.getenv("USER_NAME") + ":" + System.getenv("USER_PASSWORD")))
-                    authenticated = true;
-            }
-            if (!authenticated) {
-                response.header("WWW-Authenticate", "Basic realm=\"Restricted\"");
-                response.status(401);
-                if (!request.pathInfo().equals("/admin/login")) {
-                    response.redirect("/admin/login");
-                }
-            }
+				String b64Credentials = auth.substring("Basic".length()).trim();
+				String credentials = new String(Base64.getDecoder().decode(b64Credentials));
+				if (credentials.equals(System.getenv("USER_NAME") + ":" + System.getenv("USER_PASSWORD")))
+					authenticated = true;
+			}
+			if (!authenticated) {
+				response.header("WWW-Authenticate", "Basic realm=\"Restricted\"");
+				response.status(401);
+				if (!request.pathInfo().equals("/admin/login")) {
+					response.redirect("/admin/login");
+				}
+			}
 		});
 
-		get("/admin/create-redirect", RedirectsControllerOld.serveCreateRedirect);
-		post("/admin/create-redirect", new CreateRedirect(redirectsModel));
-		get("/admin/redirects", new RedirectsController(redirectsModel));
-		get("/admin/edit-redirect/:id", new EditRedirectDisplay(redirectsModel));
-		post("/admin/edit-redirect/:id", new EditRedirectPost(redirectsModel));
-		post("/admin/delete-redirect/:id", new DeleteRedirect(redirectsModel));
-		get("/admin/login", (request, response) -> "login");
-		get("/", new Redirector(redirectsModel));
+		http.post("/admin/create-redirect", new CreateRedirect(redirectsModel));
+		http.get("/admin/redirects", new RedirectsController(redirectsModel));
+		http.get("/admin/edit-redirect/:id", new EditRedirectDisplay(redirectsModel));
+		http.post("/admin/edit-redirect/:id", new EditRedirectPost(redirectsModel));
+		http.post("/admin/delete-redirect/:id", new DeleteRedirect(redirectsModel));
+		http.get("/admin/login", (request, response) -> "login");
+	}
+
+	static void startRedirectInstance() {
+		Service http = ignite().port(3480);
+		http.get("/*", new Redirector(redirectsModel));
+		http.exception(Exception.class, (exception, request, response) -> {
+			exception.printStackTrace();
+		});
 	}
 }
